345ba134ae2627fcbc48432dc3549f510cf9b718
syooon <duri600@gmail.com>
2020-06-17 17:19:16 +0900

    2206_breaking_wall solving...

---
+++ boj_c++_code/BFS_DFS/2206_breaking_wall/2206_breaking_wall/2206_breaking_wall.cpp
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int N, M;
+int temp;
+int Map[1000][1000];
+int visit[1000][1000][2];
+
+typedef pair<int, int> P;
+typedef pair<P, int> status;
+queue<status> q;
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
+
+void InputData() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			//cin >> temp;
+			scanf("%1d", &temp);
+			Map[i][j] = temp;
+		}
+	}
+}
+
+
+int BFS(status s) {
+	q.push(s);
+	visit[s.first.first][s.first.second][s.second] = 1;
+
+	while (!q.empty())
+	{
+		int x = q.front().first.first;
+		int y = q.front().first.second;
+		int block = q.front().second;
+		q.pop();
+
+		if (y == N && x == M) {
+			return visit[y][x][block];
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			int next_x = x + dx[i];
+			int next_y = y + dy[i];
+
+			if (next_x >= M || next_x < 0 || next_y >= N || next_y < 0) {
+				continue;
+			}
+
+			if ((Map[next_y][next_x] == 1) && block) {
+				visit[next_y][next_x][block - 1] = visit[y][x][block] + 1;
+				q.push({ { next_y,next_x }, block - 1 });
+			}
+			
+			if ((Map[next_y][next_x] == 0) && !visit[next_y][next_x][block])
+			{
+				visit[next_y][next_x][block] = visit[y][x][block] + 1;
+				q.push({ {next_y,next_x}, block });
+			}
+
+
+		}
+	}
+	return -1;
+	
+}
+
+
+int main(void) {
+	int ans;
+	InputData();
+	P start = { 0,0 };
+	status s = { start,1 };
+	ans = BFS(s);
+
+	cout << ans;
+	return 0;
+}
