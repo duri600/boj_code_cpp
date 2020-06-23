cf99c4893ba22859f43dad7342eeaacd97b3b3b1
syooon <duri600@gmail.com>
2020-06-24 00:11:17 +0900

    1003_fibonacci_dp solved

--- boj_c++_code/dynamic_programming/1003_fibonacci/1003_fibonacci/1003_fibonacci.cpp
+++ boj_c++_code/dynamic_programming/1003_fibonacci/1003_fibonacci/1003_fibonacci.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 using namespace std;
 
+int ans[41];
+
+int fibonacci(int n) {
+
+	if (n <= 1) {
+		return ans[n] = 1;
+	}
+	else
+	{
+		if (ans[n] > 0)
+		{
+			return ans[n];
+		}
+	}
+
+
+	return ans[n] = fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int T;
 int N;
 int main(void) {
-    cin >> T;
+	cin >> T;
 
-    for (int i = 0; i < T; i++)
-    {
-        cin >> N;
-    }
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N;
+		if (N == 0) {
+			cout << "1" << " " << "0" << endl;
+		}
+
+		else if (N == 1) {
+			cout << "0" << " " << "1" << endl;
+		}
+
+		else
+		{
+			cout << fibonacci(N - 2) << " " << fibonacci(N - 1) << endl;
+		}
+
+	}
+
+	return 0;
 }
 
-int fibonacci(int n) {
-    if (n == 0) {
-        cout << "0" << endl;
-        return 0;
-    }
-    else if(n == 1)
-    {
-        cout << "1" << endl;
-        return 1;
-    }
-    else
-    {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
