fb6886ee6562d50b44515232976e895117804fed
syooon <duri600@gmail.com>
2020-08-27 17:25:25 +0900

    math 1330_compare_solve

---
+++ boj_c++_code/math/1330_compare/1330_compare/1330_compare.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int comp_val(int a, int b)
+{
+	if (a > b)
+	{
+		return 0;
+	}
+	else if (a < b)
+	{
+		return 1;
+	}
+	else if (a == b)
+	{
+		return 2;
+	}
+}
+
+int main(void)
+{
+	int a, b;
+	cin >> a >> b;
+	int ans = comp_val(a, b);
+	switch (ans)
+	{
+	case 0:
+		cout << ">" << endl;
+		break;
+	case 1:
+		cout << "<" << endl;
+		break;
+	case 2:
+		cout << "==" << endl;
+		break;
+	default:
+		break;
+	}
+	return 0;
+}
